c318955e5f7302bcbd542bdff1718dbde8c03e26
artur.cqueiroz <arturos.queiroz@gmail.com>
2025-11-25 15:14:21 -0300

    Alterando a barra de pesquisa

--- Views/AcessibilidadePage.xaml.cs
+++ Views/AcessibilidadePage.xaml.cs
@@ -10,4 +10,19 @@ public partial class AcessibilidadePage : ContentPage
 	{
 		await Navigation.PushAsync(new TelaPrincipal());
     }
+    public async void MenuPrincipla(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MenuPrincipal());
+    }
+    public void Pesquisar(object sender, EventArgs e)
+    {
+        string termo = BarraDePesquisa.Text;
+
+        if (string.IsNullOrWhiteSpace(termo))
+        {
+            return;
+        }
+
+        DisplayAlert("Busca", $"Você pesquisou por: {termo}", "OK");
+    }
 }
